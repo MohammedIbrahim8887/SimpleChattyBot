f624d09726faa0b91edb4abc2835355c48d4c43f
MohammedIbrahim8887 <95257247+MohammedIbrahim8887@users.noreply.github.com>
2022-11-21 12:21:15 +0300

    First Push

---
+++ Topics/Standard input/Read and print/src/Main.kt
@@ -0,0 +1,13 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val hello = scanner.next()
+    val kotlin = scanner.next()
+    val num1 = scanner.nextInt()
+    val num2 = scanner.nextInt()
+    println(hello)
+    println(kotlin)
+    println(num1)
+    println(num2)
+}
